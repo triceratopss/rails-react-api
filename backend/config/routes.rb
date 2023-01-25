234385ba0260bdf7bbd0877000140c9fbc0f9909
triceratopss <96670142+triceratopss@users.noreply.github.com>
2023-01-25 16:48:41 +0900

    add: routing

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # ヘルスチェック
+  get '/health_check', to: 'health_checks#index'
+  # 記事一覧
+  get '/blogs', to: 'blogs#index'
+  # 記事詳細
+  get '/blogs/:id', to: 'blogs#show'
+  # 記事作成
+  post '/blogs', to: 'blogs#create'
 end
