e175a2ff2de2dcfe7c7e234582b3f4c05540a13a
triceratopss <96670142+triceratopss@users.noreply.github.com>
2023-01-26 09:52:53 +0900

    feat: create blog

--- frontend/src/routes/createBlog.tsx
+++ frontend/src/routes/createBlog.tsx
@@ -1,6 +1,17 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { axiosInstance } from '../utils/axios.js'
 
 function CreateBlog() {
+  const [title, setTitle] = useState('')
+  const [contents, setContents] = useState('')
+  const navigate = useNavigate()
+
+  const onClick = async () => {
+    await axiosInstance.post('/blogs', { title, contents })
+    navigate('/blogs', { replace: true })
+  }
+
   return (
     <div style={{ margin: 'auto', width: '1000px' }}>
       <h1>ブログ作成画面</h1>
@@ -9,6 +20,23 @@ function CreateBlog() {
       </div>
       <div>
         <Link to="/blogs">記事一覧画面</Link>
+      </div>
+
+      <h2>タイトルを入力してください</h2>
+      <div>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} style={{ width: '500px' }} />
+      </div>
+      <h2>本文を入力してください</h2>
+      <div>
+        <textarea
+          value={contents}
+          onChange={(e) => setContents(e.target.value)}
+          style={{ width: '500px', height: '300px' }}
+        />
+      </div>
+
+      <div style={{ marginTop: '20px' }}>
+        <button onClick={onClick}>記事を作成する</button>
       </div>
     </div>
   )
